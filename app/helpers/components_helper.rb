e5d07a9eed0a177616003ff3f9b7ff253bc7c9a7
anne <richardson.ae@gmail.com>
2017-11-19 11:41:29 -0600

    Refactor component name/manufacturer into helper

--- app/helpers/components_helper.rb
+++ app/helpers/components_helper.rb
@@ -3,4 +3,17 @@ module ComponentsHelper
   def display_size(component)
     "#{component.height_in}h x #{component.width_in}w"
   end
+
+  def display_component(component)
+    "#{display_name(component)} (#{display_manufacturer(component)})".html_safe
+  end
+
+  private
+  def display_name(component)
+    link_to component, component_path(component)
+  end
+
+  def display_manufacturer(component)
+     link_to component.manufacturer, manufacturer_path(component.manufacturer)
+  end
 end
