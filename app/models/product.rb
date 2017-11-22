7e1a2f42d8c41d8933e959ce61a6791a8b1e9455
anne <richardson.ae@gmail.com>
2017-11-22 16:01:40 -0600

    Adds components to products breakdown

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,10 @@ class Product < ApplicationRecord
   has_many :component_products, dependent: :destroy
   has_many :components, through: :component_products
 
+  def cost_each
+    total_cost / qty
+  end
+
   def total_cost
     prices = components.map do |c|
       c.cost_for_qty(qty)
